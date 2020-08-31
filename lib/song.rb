61d09a76c6ca99316602baac9a2dae75cbb66f99
Hugo Rodriguez <hugor37@gmail.com>
2020-08-31 08:32:51 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq # .uniq method returns the array with NO duplicates
+    end
+
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_count = Hash.new(0)
+        @@genres.each { |genre| genre_count[genre] += 1 }
+        genre_count
+    end
+
+    def self.artist_count
+        artist_count = Hash.new(0)
+        @@artists.each { |artist|artist_count[artist] += 1 }
+        artist_count
+    end
+
+end
